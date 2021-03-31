efdc6ce687e8c77d3c22569aa73d1b58f4c9de76
MIDAS <MIDAS26@users.noreply.github.com>
2021-04-01 01:33:08 +0530

    Adding Files

---
+++ mainclass.java
@@ -0,0 +1,9 @@
+import package1.*;
+
+public class mainclass {
+    public static void main(String[] args) {
+        new startpage();
+    }
+}
+//    String[] a = {"Ankit Choudhary","Mittul Daswani","Aarya Devanai"};
+//    String[] b={"4375 1111 2222 1234","1234 5678 9012 3456","4322 1232 0000 0001"};
